87372ea485a901f400c291fba9375e4c0fc8125a
Michael <mnolan@mit.edu>
2014-05-30 11:41:24 -0400

    Add the dlib library (note that some of these files are redundant to the current project and may be removed later)

---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+/*	--------------------------------------------------------------------------
+
+	Automated Sleep Scoring (Offline)
+	
+	Michael Nolan
+	Neurostat Lab
+	MIT
+	
+	Created:		30/5/2014
+	Last Updated:	30/5/2014
+	
+	--------------------------------------------------------------------	*/
+	
+using namespace std
+
